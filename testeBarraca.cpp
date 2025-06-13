6d3ef2c71a5e576b6f131b8ba4c0d301f114dc2d
Mayara <mayaradamazio@tecgraf.puc-rio.br>
2025-06-13 17:18:38 -0300

    WIP1

--- testeBarraca.cpp
+++ testeBarraca.cpp
@@ -1,60 +1,20 @@
-/* #include "testeBarraca.h"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include "barraca.h"
-#include <map>
-#include <iostream>
 
-struct produto{
-    std::string nome;
-    size_t id;
-};
-
-
-int main()
+TESTE_CASE("Testando função cria barraca")
 {
-    std::map<size_t, std::string> lProdutos;
-    lProdutos.emplace(1, "Pão de Queijo");
-    lProdutos.emplace(2, "Mate");
+    auxBarraca barracaNova{"a", 8, 30, 19, 30, 1234};
 
-    std::cout << "TESTE 1: ler todas as barracas com a lista vazia - Correto 1" << std::endl;
-    auto resultado1 = mostrarTodasBarracasEProdutos();
-    if (resultado1)
-        std::cout << "Passou no teste 1" << std::endl;
-    else
-        std::cout << "Não passou no teste 1" << std::endl;
+    SUBCASE("Cria barraca nova")
+    {
+        CHECK(criaBarraca(barracaNova) == 0);
+    }
 
-    auxBarraca barraca1{"b1", 8, 30, 19, 30, 1234};
-
-    std::cout << "TESTE 2: Adicionar uma barraca no map - Correto 0" << std::endl;
-
-    auto resultado2 = criaBarraca(barraca1);
-    if (!resultado1)
-        std::cout << "Passou no teste 2" << std::endl;
-    else
-        std::cout << "Não passou no teste 2" << std::endl;
-
-     std::cout << "TESTE 3: Adicionar uma barraca no map - Correto 0" << std::endl;
-
-    auto resultado2 = criaBarraca(barraca1);
-    if (!resultado1)
-        std::cout << "Passou no teste 2" << std::endl;
-    else
-        std::cout << "Não passou no teste 2" << std::endl;
-
-    
-
-
-
-
-
-
-
-
-    
+    SUBCASE("Tentar inserir barraca com id já existente")
+    {
+        CHECK(criaBarraca(barracaNova) == 1);
+    }
 }
 
-
-
-
-int buscaProduto(size_t id, AuxProduto auxProduto){
-
-} */
+TEST_CASE
